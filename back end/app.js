444ea449f11f2f365f72663a2ac2ea1fc382f371
Hamzaoui-Louai <l_hamzaoui@estin.dz>
2025-07-01 17:56:37 +0100

    mongo db set up

--- back end/app.js
+++ back end/app.js
@@ -1,10 +1,16 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 const PORT = 3000;
 
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('✅ Connected to MongoDB Atlas'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
 
-
-
+app.get("/",(req,res)=>{res.send("everything working well")})
 
 app.listen(PORT,()=>{ console.log(`server is running on ${PORT}`)})
